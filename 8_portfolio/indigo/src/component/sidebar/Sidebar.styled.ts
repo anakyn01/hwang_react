c8fd400f1e561f8c908914540895a7261b7823e6
hwang <anakyn@naver.com>
2026-07-22 10:55:06 +0900

    3교시

--- 8_portfolio/indigo/src/component/sidebar/Sidebar.styled.ts
+++ 8_portfolio/indigo/src/component/sidebar/Sidebar.styled.ts
@@ -12,3 +12,39 @@ list-style:none;
 display:flex;
 flex-direction:column;
 `;
+
+export const SidebarBrand = styled.a`
+height:4.375rem;
+text-decoration:none;
+font-size:1rem;
+font-weight:800;
+padding:1.5rem 1rem;
+text-align:center;
+text-transform:uppercase;
+letter-spacing:0.05rem;
+z-index:1; color:#fff;
+display:flex; align-items:center;
+justify-content:center;
+&:hover{
+color:#fff; text-decoration:none;
+}
+`;
+
+export const NavItem = styled.li`
+position:relative;
+`;
+
+export const NavLink = styled.a`
+display:block; width:100%; text-align:left;
+padding:1rem; width:14rem; 
+color:rgba(255, 255, 255, .8);
+font-weight:700;
+text-decoration:none;
+&:hover{color: #fff;}
+svg,i{margin-right:0.25rem;}
+`;
+
+export const Divider = styled.hr`
+margin:0 1rem 1rem;
+border-top:1px solid rgba(255,255,255,0.15);
+`;
--- 8_portfolio/indigo/src/component/sidebar/Sidebar.tsx
+++ 8_portfolio/indigo/src/component/sidebar/Sidebar.tsx
@@ -1,5 +1,32 @@
-export const Sidebar = () => {
+import React from 'react';
+import { SidebarContainer,
+ SidebarBrand, NavItem, NavLink, Divider   
+ } from './Sidebar.styled';
+
+export const Sidebar:React.FC = () => {
     return(
-        <></>
+        <>
+<SidebarContainer 
+className='sidebar sidebar-dark accordion'
+>
+    <SidebarBrand href="/">
+    <div className='sidebar-brand-icon rotate-n-15'>
+        <i className='fas fa-laugh-wink'></i>
+    </div>
+    <div className='sidebar-brand-text mx-3'>
+        Admin <sup>2</sup>
+    </div>
+    </SidebarBrand>
+    <Divider className='my-0'/>
+    <NavItem className='active'>
+        <NavLink href="/">
+        <i className='fas fa-fw fa-tachometer-alt'></i>
+        </NavLink>
+    </NavItem>
+
+    <Divider/>
+
+</SidebarContainer>
+        </>
     )
 }
