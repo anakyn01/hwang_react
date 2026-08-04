b8fa78781ad3087cdc107b88c7c2cbda333c5f6f
hwang <anakyn@naver.com>
2026-08-04 16:15:30 +0900

    next

--- 6_boot/workspace/0_java/advanced/src/collect/ArrayListt.java
+++ 6_boot/workspace/0_java/advanced/src/collect/ArrayListt.java
@@ -2,6 +2,7 @@ package collect;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /*
 List컬렉션은 객체 자체를 저장하는 것이 아니라 객체의
 번지를 저장합니다
@@ -25,5 +26,31 @@ void clear() 저장된 모든 객체를 삭제
 E remove(int index)주어진 인덱스에 저장된 객체를 삭제
 boolean remove(Object o)주어진 객체를 삭제
 * */
-public class ArrayList {
+public class ArrayListt {
+    public static void main(String[] args) {
+// 1. String 객체의 '번지(주소)'를 저장할
+// List 컬렉션 생성 (보통 ArrayList를 가장 많이 사용합니다)
+        List<String> list = new ArrayList<>();
+        // 2. isEmpty():
+System.out.println("처음 리스트가 비어있나요? " + list.isEmpty());
+//객체추가 3. add(E e)
+        list.add("사과");
+        list.add("바나나");
+        list.add("사과");
+//null 저장: 객체가 없다는 의미인
+// null도 하나의 칸을 차지하며 저장될 수 있습니다.
+        list.add(null);
+//add(int index, E element)
+list.add(1, "포도");
+list.set(2,"딸기");
+System.out.println(list);
+System.out.println("총 객체 수: " + list.size());
+System.out.println("1번 인덱스의 과일: " + list.size());
+System.out.println("1번 인덱스의 과일: " + list.get(1));
+System.out.println("사과가 있나요? : " + list.contains("사과"));
+list.remove(4);
+list.remove("사과");
+list.clear();
+System.out.println("clear후 리스트가 비어 있나요? " + list.isEmpty());
+    }
 }
--- 6_boot/workspace/0_java/advanced/src/collect/Intro.java
+++ 6_boot/workspace/0_java/advanced/src/collect/Intro.java
@@ -2,3 +2,22 @@ package collect;
 
 public class Intro {
 }
+/*
+컬렉션 프레임워크
+자바는 널리 알려져 있는 자료구조(Data Structure)를
+바탕으로 객체들을 효율적으로 추가,삭제,검색 할수 있는
+인터페이스와 클래스를 java.util패키지에 포함시커 놓았다
+컬렉션 프레임워크는 몇가지 인터페이스를 통해서
+다양한 컬렉션 클래스를 이용할수 있도록 설계되어 있다
+주요 인터페이스에는
+List {순서르 유지하고 저장, 중복가능}
+
+[ArrayList, Vector, LinkedList]
+Set {순서르 유지하지 않고 저장, 중복 저장 안됨}
+
+[HashSet, TreeSet]
+Map {키 와 값으로 저장된 엔트리 저장, 중복저장 안됨}
+[HashMap, Hashtable, TreeMap, Properties]
+
+추후21에서는 순차컬렉션 {sequenced Collection}
+* */
