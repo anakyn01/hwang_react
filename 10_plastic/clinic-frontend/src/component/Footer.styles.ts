e0e9a7ec30513e237546ba5e28f4c5039a8de64b
hwang <anakyn@naver.com>
2026-08-21 17:41:32 +0900

    endgame

---
+++ 10_plastic/clinic-frontend/src/component/Footer.styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer``;
+export const FooterInner = styled.div``;
+export const TopSection = styled.div``;
+export const CsInfo = styled.div``;
+export const PhoneNumber = styled.div``;
+export const CsTitle = styled.div``;
+export const ScheduleWrapper = styled.div``;
+export const ScheduleBlock = styled.div``;
+export const ScheduleTitle = styled.div``;
+export const ScheduleText = styled.div``;
+export const LocationButton = styled.button``;
+
+export const BottomSection = styled.div``;
+export const CompanyInfo = styled.div``;
+export const CompanyName = styled.h2``;
+export const InfoText = styled.p``;
+export const BottomRight = styled.div``;
+export const PolicyButtons = styled.div``;
+export const PolicyBtn = styled.button``;
+export const FamilySiteTitle = styled.div``;
+export const FamilySiteLogos = styled.div``;
+
+//floating
+export const FlaotingMenu = styled.div``;
+export const FabItem = styled.div``;
+export const FabIcon = styled.div<{$bgColor:string}>``;
+export const FabText = styled.span``;
+
