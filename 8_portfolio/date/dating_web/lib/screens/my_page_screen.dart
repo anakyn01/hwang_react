33de8c2477071fbece872e7a30a59b41935796cb
hwang <anakyn@naver.com>
2026-08-28 15:08:45 +0900

    mypage

--- 8_portfolio/date/dating_web/lib/screens/my_page_screen.dart
+++ 8_portfolio/date/dating_web/lib/screens/my_page_screen.dart
@@ -131,12 +131,73 @@ void _askPhotoPermission(voidCallback onSuccess){
               print("사진 권한 승인됨");
               Navigator.pop(context);
               onSuccess();
-            }
+            },
             child:const Text('허용',style:TextStyle(color:Colors.white)),
             ),
       ],
       ),
 );
+}
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    backgroundColor: bgColor,
+    appBar:AppBar(// 화면 맨 위 상단바 (지붕)
+backgroundColor: bgColor,
+elevation: 0,
+title:const Text('마이페이지', style:TextStyle(
+fontSize:22, fontWeight:FontWeight.bold, 
+color:Colors.white)),
+actions:[
+  icon:const Icon(Icons.settings, color:Colors.white70),
+  onPressed:(){
+    print("설정클릭");
+  },
+    ),
+],
+), 
+body:SingleChildScrollView(
+  padding:const EdgeInsets.all(20.0),
+  child:Column(
+ children: [
+_buildProfileCard(), const SizedBox(height:24),
+_buildPremiumBanner(), const SizedBox(height:24),
+_builMenuList(), const Sizedbox(height:40),
+ ],
+ ),
+),
+bottomNavigationBar: _buildBottomNav(),
+);
+}
+
+Widget _buildProfileCard(){
+  return Container();
+}
+
+Widget _buildPremiumBanner(){
+  return Container();
+}
+
+Widget _buildMenuList(){
+  return Container();
+}
+
+//조립용 도구
+Widget _buildStatItem(String number, String label){
+  return Column();
+}
+
+Widget _buildVerticalDivider(){
+  return Container();
+}
+
+Widget _buildBottomNav(){
+  return Theme();
+}
+
+BottomNavigationBarItem _buildBottomNavItem(String lable, IconData icon, int index){
+  return BottomNavigationBarItem(icon: icon);
 }
 
 }
