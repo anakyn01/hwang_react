253173f6da9c37b090a34a97a49d70ab3b5784a3
hwang <anakyn@naver.com>
2026-09-11 11:36:43 +0900

    2차 회원가입

--- 8_portfolio/date/dating_web/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 8_portfolio/date/dating_web/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import flutter_inappwebview_macos
+import geocoding_darwin
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
