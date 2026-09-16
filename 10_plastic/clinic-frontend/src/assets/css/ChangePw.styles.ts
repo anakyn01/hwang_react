c985fd7f1e137e8fbaa7c8c21a14247a6506b8f4
hwang <anakyn@naver.com>
2026-09-16 12:59:32 +0900

    Common

--- 10_plastic/clinic-frontend/src/assets/css/ChangePw.styles.ts
+++ 10_plastic/clinic-frontend/src/assets/css/ChangePw.styles.ts
@@ -1,8 +1,28 @@
 import styled from 'styled-components';
+import { FlexCenter, BoxShadow, FlexBetween, 
+    BlueButtonTheme, LinearGradient } from './Common.styles';
 
-export const Wrapper= styled.div``;
-export const Card= styled.div``;
-export const Header= styled.header``;
+export const Wrapper= styled.div`
+background-color:#4e73df;
+${LinearGradient};
+background-size:cover;
+min-height:100vh;
+${FlexCenter};
+padding:1.4rem;
+`;
+export const Card= styled.div`
+background-color:#fff;
+border:none;
+border-radius:0.35rem;
+${BoxShadow};
+width:100%;
+max-width:500px;
+padding:3rem;
+`;
+export const Header= styled.header`
+text-align:center;
+margin-bottom:2rem;
+`;
 
 export const Description= styled.p``;
 export const Form= styled.form``;
